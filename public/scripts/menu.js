0a28569740b6416ddbf3ad27a69f17fca7789a13
Gean <coder.gean@gmail.com>
2020-11-16 12:55:02 -0200

    mobile layout

---
+++ public/scripts/menu.js
@@ -0,0 +1,6 @@
+const menu_bars = document.querySelector('.fa-bars')
+
+menu_bars.addEventListener('click', () => {
+
+    document.querySelector('.menu-list').classList.add('.animation-menu-appear')
+})
