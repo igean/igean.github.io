3628ceaff18b46dd316d5fa656e3c6f33e50471b
Gean Araujo <coder.gean@gmail.com>
2020-11-18 10:27:01 -0300

    menu functions

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,11 +1,31 @@
+// menu animation
 const menu_bars = document.querySelector('.fa-bars')
 const menu = document.querySelector('.menu-list')
+const menu_item = document.querySelectorAll('li a.menu-item')
 
+// open and close menu
 menu_bars.addEventListener('click', () => {
 
     if (menu.classList.toString().includes('menu-hidden')) {
-        menu.classList.remove('menu-hidden')
+        open_menu()
+        document.querySelector('main').style.pointerEvents = 'none';
+        
     }else {
-        menu.classList.add('menu-hidden')
+        close_menu()
     }
 })
+
+for (item of menu_item) {
+    item.addEventListener('click', () => {
+        close_menu()
+    })
+}
+
+// all functions go here
+function close_menu() {
+    menu.classList.add('menu-hidden')
+}
+
+function open_menu() {
+    menu.classList.remove('menu-hidden')
+}
